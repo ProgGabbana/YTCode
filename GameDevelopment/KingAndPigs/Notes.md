16f74ca3a14ad3c508877b4e6b24815943b7164f
ProgGabbana <fasciani.rider@gmail.com>
2026-04-01 16:48:59 +0200

    GameDevelopment 01-04/2026

---
+++ GameDevelopment/KingAndPigs/Notes.md
@@ -0,0 +1,7 @@
+# Project Overview
+
+This project is based on the [Kings and Pigs](https://github.com/chriscourses/kings-and-pigs) tutorial by [Chris Courses](https://www.youtube.com/watch?v=Lcdc2v-9PjA). You can follow the video or refer to the repository for the initial setup. 
+
+This file serves as a space for personal notes and documentation regarding the program's development.
+
+For this project we're coging to use Tiled, that you can download from [here](https://www.mapeditor.org/)
---
+++ GameDevelopment/KingAndPigs/js/event_listeners.js
@@ -0,0 +1,26 @@
+window.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    switch (event.key) {
+        case 'w':
+            if (player.velocity.y === 0) player.velocity.y -= 20
+            break
+        case 'a': // Move left
+            keys.a.pressed = true
+            break
+        case 'd': // Move right
+            keys.d.pressed = true
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    console.log(event.key)
+    switch (event.key) {
+        case 'a': // Move left
+            keys.a.pressed = false
+            break
+        case 'd': // Move right
+            keys.d.pressed = false
+            break
+    }
+})
