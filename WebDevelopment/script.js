2380ed501abe9f9a0201d9a8c8e3796f57da4343
ProgGabbana <fasciani.rider@gmail.com>
2026-04-08 12:10:46 +0200

    08-04-2026

--- WebDevelopment/script.js
+++ WebDevelopment/script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -7,8 +8,14 @@ menuOpenButton.addEventListener("click", () => {
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 
+navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", () => menuOpenButton.click());
+});
+
 const swiper = new Swiper('.slider-wrapper', {
     loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
 
     // If we need pagination
     pagination: {
